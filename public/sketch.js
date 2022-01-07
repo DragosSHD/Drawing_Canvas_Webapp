2f7413a7be03822577add5a14e9b1ae4b99ff24f
DragosSHD <dragos.dragos.502@gmail.com>
2022-01-07 18:43:07 +0200

    Manage sending and receiving data through sockets.

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,31 @@
+let socket;
+
 function setup() {
     createCanvas(1500, 750);
+    background(51);
+
+    socket = io.connect('http://localhost:3000');
+    socket.on('mouse', newDrawing);
+
 }
 
-function draw() {
-    background(51);
+function newDrawing(data) {
+    noStroke();
+    fill(150);
+    ellipse(data.x, data.y, 60, 60);
+}
+
+function mouseDragged() {
+    noStroke();
+    fill(255);
     ellipse(mouseX, mouseY, 60, 60);
+
+
+    let data = {
+        x: mouseX,
+        y: mouseY
+    }
+
+    socket.emit('mouse', data);
+
 }
--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-
+// import express
 let express = require('express');
-
+// create express app
 let app = express();
-let server = app.listen(3000);
+let server = app.listen(3000); // start server listening on port 3000
 
-app.use(express.static('public'));
+app.use(express.static('public')); // specify root directory
 
 console.log("My socket server is running");
+
+let { Server: socket } = require('socket.io');
+
+let io = new socket(server);
+
+io.sockets.on('connection', (socket) => {
+    console.log('new connection: ' + socket.id);
+
+    socket.on('mouse', (data) => {
+        socket.broadcast.emit('mouse', data);
+    });
+
+});
