bee15f30f0f7f637699291b8ce5ef43921879ba5
DragosSHD <dragos.dragos.502@gmail.com>
2022-01-08 15:03:50 +0200

    Change brightness for other users drawings.

--- public/sketch.js
+++ public/sketch.js
@@ -37,7 +37,8 @@ let sketch = function(p) {
 
     function newDrawing(data) {
         p.noStroke();
-        p.fill(150);
+        let newColor = increase_brightness(data.color, 50);
+        p.fill(newColor);
         p.ellipse(data.x, data.y, 60, 60);
     }
 
@@ -49,11 +50,31 @@ let sketch = function(p) {
 
         let data = {
             x: p.mouseX,
-            y: p.mouseY
+            y: p.mouseY,
+            color: fillColor
         }
 
         socket.emit('mouse', data);
     }
+}
+
+function increase_brightness(hex, percent){
+    // strip the leading # if it's there
+    hex = hex.replace(/^\s*#|\s*$/g, '');
+
+    // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
+    if(hex.length == 3){
+        hex = hex.replace(/(.)/g, '$1$1');
+    }
+
+    var r = parseInt(hex.substr(0, 2), 16),
+        g = parseInt(hex.substr(2, 2), 16),
+        b = parseInt(hex.substr(4, 2), 16);
+
+    return '#' +
+        ((0|(1<<8) + r + (256 - r) * percent / 100).toString(16)).substr(1) +
+        ((0|(1<<8) + g + (256 - g) * percent / 100).toString(16)).substr(1) +
+        ((0|(1<<8) + b + (256 - b) * percent / 100).toString(16)).substr(1);
 }
 
 new p5(sketch, window.document.getElementById('drawing-board'));
