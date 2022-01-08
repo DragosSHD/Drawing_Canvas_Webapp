eb04f2a2ee3a9d0d9af49186cd164b5dc0f6bad5
DragosSHD <dragos.dragos.502@gmail.com>
2022-01-08 14:39:27 +0200

    Set FE in place

--- public/sketch.js
+++ public/sketch.js
@@ -1,31 +1,35 @@
 let socket;
 
-function setup() {
-    createCanvas(1500, 750);
-    background(51);
+let sketch = function(p) {
+    p.setup = function() {
+        p.createCanvas(1200, 750);
+        p.background(51);
 
-    socket = io.connect('http://localhost:3000');
-    socket.on('mouse', newDrawing);
+        socket = io.connect('http://localhost:3000');
+        socket.on('mouse', newDrawing);
 
-}
-
-function newDrawing(data) {
-    noStroke();
-    fill(150);
-    ellipse(data.x, data.y, 60, 60);
-}
-
-function mouseDragged() {
-    noStroke();
-    fill(255);
-    ellipse(mouseX, mouseY, 60, 60);
-
-
-    let data = {
-        x: mouseX,
-        y: mouseY
     }
 
-    socket.emit('mouse', data);
+    function newDrawing(data) {
+        p.noStroke();
+        p.fill(150);
+        p.ellipse(data.x, data.y, 60, 60);
+    }
+
+    p.mouseDragged = function() {
+        p.noStroke();
+        p.fill(255);
+        p.ellipse(p.mouseX, p.mouseY, 60, 60);
+
+
+        let data = {
+            x: p.mouseX,
+            y: p.mouseY
+        }
+
+        socket.emit('mouse', data);
+    }
 }
+
+new p5(sketch, window.document.getElementById('drawing-board'));
 
